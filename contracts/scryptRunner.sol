3bec2166c5e0b5071500043a810e9c0739b05c3f
bloodstalker <thabogre@gmail.com>
2017-10-07 03:37:20 +0330

    doxy comments

--- contracts/scryptRunner.sol
+++ contracts/scryptRunner.sol
@@ -2,12 +2,30 @@ pragma solidity ^0.4.0;
 
 import {ScryptFramework} from "./scryptFramework.sol";
  
-
+/**
+  * @title 
+  * @author Christian Reitwiessner
+*/
 contract ScryptRunner is ScryptFramework {
+  /**
+    * @dev reserve 4096 bytes for the fullMemory member of the State struct
+  *
+    * @param state the State struct instance
+  *
+    * @return none
+  */
     function initMemory(State memory state) pure internal {
         state.fullMemory = new uint[](4 * 1024);
     }
 
+    /**
+      * @dev run the verification
+    *
+      * @param input the input
+      * @param upToStep which step to stop running at
+    *
+      * @return the state variables, the memoryHash, the merkle proof and the output byte array
+    */
     function run(bytes input, uint upToStep) pure public returns (uint[4] vars, bytes32 memoryHash, bytes32[] proof, bytes output) {
         State memory s = inputToState(input);
         Proofs memory proofs;
@@ -28,6 +46,15 @@ contract ScryptRunner is ScryptFramework {
 
     // The proof for reading memory consists of the values read from memory
     // plus a list of hashes from leaf to root.
+    /**
+      * @dev read memory and update proofs
+    *
+      * @param state the State struct instance
+      * @param index the index at which to read from fullMemory
+      * @param proofs the merkle proofs for the read
+    *
+      * @return returns the values read from fullMem
+    */
     function readMemory(State memory state, uint index, Proofs memory proofs) pure internal returns (uint a, uint b, uint c, uint d) {
         require(index < 1024);
         uint pos = 0x20 * 4 * index;
@@ -50,8 +77,19 @@ contract ScryptRunner is ScryptFramework {
             proofs.proof[3] = bytes32(d);
         }
     }
+
     // The proof for writing to memory consists of the four old values in
     // memory followed by a list of hashes from leaf to root.
+    /**
+      * @dev write to memory
+    *
+      * @param state the State struct
+      * @param index the index at which the write is perfomed
+      * @param values the values that are going to be written in fullMemory
+      * @param proofs the proofs to be updated
+    *
+      * @return none
+    */
     function writeMemory(State memory state, uint index, uint[4] values, Proofs memory proofs) pure internal {
         require(index < 1024);
         uint pos = 0x20 * 4 * index;
@@ -83,12 +121,21 @@ contract ScryptRunner is ScryptFramework {
             proofs.proof[3] = bytes32(oldValues[3]);
         }
     }
+
     // Generate a proof that shows that the memory root hash was updated correctly.
     // Returns the value stored at the index (4 array elemets) followed by
     // a list of siblings (from leaf to root) and the new root hash.
     // This assumes that index is multiplied by four.
     // Since we know that memory is only written in sequence, this might be
     // optimized, but we keep it general for now.
+    /**
+      * @dev generate proof that shows the memory root has been updated correctly
+    *
+      * @param fullMem full memory
+      * @param index the index of the value to be retunred
+    *
+      * @return the merkle proof and the value stored at index
+    */
     function generateMemoryProof(uint[] fullMem, uint index) internal pure returns (bytes32[] proof, bytes32) {
         uint access = index;
         proof = new bytes32[](14);
