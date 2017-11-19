1bf3efde962a441808c747c8f6a773e6cc968915
hswick <hswick@smu.edu>
2017-11-19 15:20:56 -0500

    Added checkScrypt test. currently broken

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
-
 const ClaimManager = artifacts.require("./claimManager.sol");
 const ScryptVerifier = artifacts.require("./scryptVerifier.sol");
+const DogeRelay = artifacts.require("./dogeRelay.sol");
 
 module.exports = function(deployer, network, thing) {
+	deployer.deploy(ScryptVerifier);
+    deployer.deploy(DogeRelay);
     deployer.deploy(ClaimManager);
-    deployer.deploy(ScryptVerifier);
 };
--- test/client.js
+++ test/client.js
@@ -1,18 +1,50 @@
 var ClaimManager = artifacts.require('./claimManager.sol');
+var DogeRelay = artifacts.require('./dogeRelay.sol');
+var ScryptVerifier = artifacts.require('./scryptVerifier.sol');
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 contract('ClaimManager', function(accounts) {
-    let claimManager;
+    let claimManager, dogeRelay, scryptVerifier;
+
+    const steps = 2050;
 
     beforeEach(async () => {
-        claimManager = await ClaimManager.new()
+        dogeRelay = await DogeRelay.new()
+        scryptVerifier = await ScryptVerifier.new()
+        claimManager = await ClaimManager.new(dogeRelay.address, scryptVerifier.address);
     })
 
     describe('it works', () => {
         const claimant = accounts[1];
+        const challenger = accounts[2];
 
-        it("be true.", async () => {
+        it("challenger challenges", async () => {
+
+            await claimManager.makeDeposit({from: claimant, value: 100});
+            await claimManager.makeDeposit({from: claimant, value: 100});
+
+            //This is throwing error
+            await claimManager.checkScrypt("foo", "0x02", claimant, {from: dogeRelay.address})
+
+            log = tx.logs.find(log => log.event === 'ClaimCreated')
+            console.log(log.args.claimID);
+            claimID = log.args.claimID
+
+            await claimManager.challengeClaim(claimID)
+
+            log = tx.logs.find(log => log.event === 'NewClaim')
+            assert.equal(log.args.sessionId, claimID)
+
+            var session = await info.getSession(log.args.sessionId)
+            await scryptVerifier.query(log.args.sessionId, Math.floor(steps / 2)).send({from: challenger})
+
+            log = tx.logs.find(log => log.event === 'NewQuery')
+            assert.equal(log.args.sessionId, claimID)
+
+            var stateHash = (await info.getStateProofAndHash(inputForStep(session.medStep, session.input), session.medStep)).stateHash
+            await scryptVerifier.respond(event.returnValues.sessionId, session.medStep, stateHash).send({from: interface.account})
+
             const number = 1000;
             assert.equal(number, 1000);
         });
