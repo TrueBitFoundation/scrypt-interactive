f26a938b2bbe4da066a45af3c6b2a0a2b3030e2c
hswick <hdswick@gmail.com>
2017-12-20 11:53:05 -0600

    Commented out offending bash lines

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-function check_node_version() {
-  MIN_VERSION="7.6.0"
-  if [[ "$MIN_VERSION" == "$1" ]]; then return 0;fi
-  local IFS=.
-  VERSION="${1:1}"
-  local i ver1=($MIN_VERSION) ver2=($VERSION) 
-  for ((i=0; i < ${#ver1[@]}; i++)); do
-    if [[ 10#${ver1[i]} > 10#${ver2[i]} ]]; then return 1;fi
-    if [[ 10#${ver1[i]} < 10#${ver2[i]} ]]; then return 0;fi
-  done
-}
+# function check_node_version() {
+#   MIN_VERSION="7.6.0"
+#   if [[ "$MIN_VERSION" == "$1" ]]; then return 0;fi
+#   local IFS=.
+#   VERSION="${1:1}"
+#   local i ver1=($MIN_VERSION) ver2=($VERSION) 
+#   for ((i=0; i < ${#ver1[@]}; i++)); do
+#     if [[ 10#${ver1[i]} > 10#${ver2[i]} ]]; then return 1;fi
+#     if [[ 10#${ver1[i]} < 10#${ver2[i]} ]]; then return 0;fi
+#   done
+# }
 
-NODE_VERSION=$(node --version)
-check_node_version $NODE_VERSION
-if [[ $? == 1 ]]; then echo "node version is too old. please use v.7.6.0 or newer." && exit 1; fi
+#NODE_VERSION=$(node --version)
+#check_node_version $NODE_VERSION
+#if [[ $? == 1 ]]; then echo "node version is too old. please use v.7.6.0 or newer." && exit 1; fi
 
 ganache-cli > ./ganache-log 2>&1 &
 GANACHE_PID=$!
