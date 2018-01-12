63804087b932a3713e57fa4cc0eb71eb50a33034
hswick <hdswick@gmail.com>
2018-01-12 11:12:07 -0600

    Figured out how to test this :D

--- client/bridge-to-the-moon/util/mine.js
+++ client/bridge-to-the-moon/util/mine.js
@@ -1,7 +1,7 @@
 module.exports = (web3) => {
   function mineBlocks(n) {
     for(i = 0; i < n; i++) {
-      // console.log("Mining block " + i)
+       console.log("Mining block " + i)
       web3.currentProvider.send({jsonrpc: "2.0", method: "evm_mine", params: [], id: 0})
     }
   }
--- client/index.js
+++ client/index.js
@@ -2,12 +2,14 @@
 
 const bttm = require('./bridge-to-the-moon')
 
-const Web3 = require('web3');
+const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 const mine = require('./bridge-to-the-moon/util/mine')(web3)
 
 const serializedBlockHeader = '030162002adb34dfa6574cf127a781ecb9683ca28f911a59020628c90c72b4a3d9942233a3b905b2388b020085dbd9e03209db4493f5420336d882d0b78b54f728b8f90058f7115a2c83221a00000000'
 const testScryptHash = 'ce60a0d4a7c2223a94437d44fe4d33a30489436714d18376f9ebc5e2bd6e5682'
+
+const timeout = require('./bridge-to-the-moon/util/timeout')
 
 //This is being used as a test file for bridge-to-the-moon client
 //truffle exec client/index.js to use
@@ -29,12 +31,11 @@ module.exports = async function(callback) {
     let claimManager = await ClaimManager.new(dogeRelayAddress, scryptVerifier.address)
 
     const bridge = await require('./bridge-to-the-moon')(claimManager, scryptVerifier, scryptRunner, web3, challenger)
- 
 
-
-    await bridge.createClaim(console, serializedBlockHeader, testScryptHash, claimant, dogeRelayAddress)
-
-    mine.mineBlocks(25)
+    bridge.createClaim(console, serializedBlockHeader, testScryptHash, claimant, dogeRelayAddress)
     
-    //bridge.monitorClaims(console, true, true)
+    bridge.monitorClaims(console, true, true)
+
+    await timeout('5000')
+    mine.mineBlocks(25)
 }
