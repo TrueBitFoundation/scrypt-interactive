709a092f4fa7b0b1fce116355d52b86f96d06733
Matt Condon <mattgcondon@gmail.com>
2018-01-18 14:21:39 -0800

    fix chmod install

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,5 @@
+set -e
+
 #!/bin/bash
 
 # function check_node_version() {
@@ -5,7 +7,7 @@
 #   if [[ "$MIN_VERSION" == "$1" ]]; then return 0;fi
 #   local IFS=.
 #   VERSION="${1:1}"
-#   local i ver1=($MIN_VERSION) ver2=($VERSION) 
+#   local i ver1=($MIN_VERSION) ver2=($VERSION)
 #   for ((i=0; i < ${#ver1[@]}; i++)); do
 #     if [[ 10#${ver1[i]} > 10#${ver2[i]} ]]; then return 1;fi
 #     if [[ 10#${ver1[i]} < 10#${ver2[i]} ]]; then return 0;fi
