39db94b283f0a0b73e0946ac72a6a22cfead48d5
Matt Condon <mattgcondon@gmail.com>
2018-01-18 10:57:39 -0800

    feat: integration foundation

--- test/integration.js
+++ test/integration.js
@@ -1,21 +1,30 @@
-// spin up parity
-// spin up ganache-cli
-// deploy contracts
-// set addresses in ENV
 
+require('dotenv').config()
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_HTTP_PROVIDER))
 
+require('./helpers/chai').should()
 
-const main = async () => {
-  const bridge = await require('./bridge-to-the-moon')(web3)
-
-  // test deposit CRUD
-  // test monitorClaim and then run through the entire flow
-}
-
-main()
-  .catch((err) => {
-    console.error(err)
-    process.exit(1)
+describe('Integration!!', () => {
+  let bridge
+  before(async () => {
+    bridge = await require('../client/bridge-to-the-moon')(web3)
+    // spin up parity
+    // spin up ganache-cli
+    // deploy contracts
+    // set addresses in ENV
   })
+
+  after(async () => {
+    // teardown processes
+  })
+
+  describe('ClaimManager', () => {
+    it('does stuff', async () => {
+      (await bridge.api.getDeposit('0x0'))
+        .should.be.bignumber.equal(0)
+      // test deposit CRUD
+      // test monitorClaim and then run through the entire flow
+    })
+  })
+})
