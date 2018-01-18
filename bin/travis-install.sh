839058373b65e60ff6a6938391d372c1ad61faf6
Matt Condon <mattgcondon@gmail.com>
2018-01-18 14:28:19 -0800

    fix: updates

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd bin
+
 wget https://parity-downloads-mirror.parity.io/v1.8.5/x86_64-unknown-linux-gnu/parity
 
 chmod 755 ./parity
@@ -8,6 +10,7 @@ npm install
 npm install -g ganache-cli truffle
 
 ./parity --chain dev 2>&1 &
+
 PARITY_PID=$!
 sleep 10
 kill $PARITY_PID
