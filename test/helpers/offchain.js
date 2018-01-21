f1d6a1f646fad1040d5724a296d3f7640e30926e
Matt Condon <mattgcondon@gmail.com>
2018-01-20 20:04:14 -0800

    feat: it's working wow

--- test/helpers/offchain.js
+++ test/helpers/offchain.js
@@ -1,19 +1,45 @@
-const Web3 = require('web3')
+const path = require('path')
 const fs = require('fs')
-const deploy = require('./deploy')
 
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PARITY_PROVIDER))
+const deploy = require('./deploy')
+const Web3 = require('web3')
+
+const parityProvider = new Web3.providers.HttpProvider('http://localhost:4242')
+const parity = new Web3(parityProvider)
+
+const ScryptRunner = artifacts.require('ScryptRunner')
+ScryptRunner.setProvider(parityProvider)
 
 var account
-
 var _scryptRunner
 
 async function scryptRunner () {
-  if (!account) account = await deploy.setupAccount('0x00a329c0648769a73afac7f9381e08fb43dbea72')
+  // if (!account) {
+  //   account = await deploy.setupAccount('0x00a329c0648769a73afac7f9381e08fb43dbea72')
+  // }
 
-  if (!_scryptRunner) _scryptRunner = await deploy.deployContract(runnerCode, runnerABI, 0, account, 4000000, true)
+  // let runnerCode = Buffer.from(fs.readFileSync(path.join(__dirname, '/offchain_evm')))
+  // let runnerABI = JSON.parse(fs.readFileSync(path.join(__dirname, '/offchain_abi.json')))
 
-  return _scryptRunner
+  // if (!_scryptRunner) {
+  //   _scryptRunner = await deploy.deployContract(runnerCode, runnerABI, account, 8000000)
+  // }
+
+  console.log('start')
+
+  debugger
+
+  parity.personal.unlockAccount('0x00a329c0648769a73afac7f9381e08fb43dbea72', '')
+
+  const thisContract = parity.eth.contract(ScryptRunner.abi).new({
+    data: ScryptRunner.bytecode,
+    from: '0x00a329c0648769a73afac7f9381e08fb43dbea72',
+    gas: 4000000,
+  })
+
+  console.log('end')
+
+  return thisContract
 }
 
 module.exports = {
