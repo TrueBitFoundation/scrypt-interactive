7690dff7a18d1bcff91ba0899c341f5dec3a4caf
Sina Habibian <sinahab@gmail.com>
2018-01-31 17:13:29 -0800

    Add deposit primitive

---
+++ client/claimManager/deposit.js
@@ -0,0 +1,32 @@
+
+// @dev – check whether the user has enough deposit
+// to submit or challenge a claim.
+// @param api – the api for the deployed contracts.
+// @param who – the account's address
+// @return – boolean.
+const isDepositEnough = async (api, who) => {
+  const requiredDeposit = await api.getMinDeposit()
+  const currentDeposit = await api.getDeposit(who)
+
+  return (currentDeposit >= requiredDeposit ? true : false)
+}
+
+// @dev – make a deposit for the account.
+// @param api – the api for the deployed contracts.
+// @param who – the account's address.
+// @param amount – amount to deposit in wei.
+const makeDeposit = async (cmd, api, who, amount) => { 
+  const myBalance = await api.getBalance(who)
+
+  if (!myBalance.gte(amount)) {
+    throw new Error(`You do not have enough ETH to make a deposit of ${amount}`)
+  }
+
+  cmd.log(`Depositing ${api.web3.fromWei(amount, 'ether')} ETH along with claim.`)
+  await api.makeDeposit({from: who, value: amount})
+}
+
+module.exports = {
+  isDepositEnough: isDepositEnough,
+  makeDeposit: makeDeposit
+}
---
+++ test/deposit.js
@@ -0,0 +1,36 @@
+
+require('dotenv').config()
+require('./helpers/chai').should()
+web3.eth.defaultAccount = web3.eth.accounts[0]
+
+const getContracts = require('../client/util/getContracts')
+
+const makeDeposit = require('../client/claimManager/deposit').makeDeposit
+const isDepositEnough  = require('../client/claimManager/deposit').isDepositEnough
+
+describe('deposit', function () {
+  let me = web3.eth.accounts[1]
+  let client
+
+  before(async () => {
+     const contracts = await (await getContracts(web3)).deploy()
+     client = await require('../client')(web3, contracts)
+  })
+
+  it('it determines whether the deposit is enough, and makes a deposit', async () => {
+    let depositAmount = await client.api.getDeposit(me)
+    assert.equal(depositAmount, 0)
+
+    let isEnough = await isDepositEnough(client.api, me)
+    assert.isFalse(isEnough)
+
+    // make a deposit
+    await makeDeposit(console, client.api, me, 2)
+
+    depositAmount = await client.api.getDeposit(me)
+    assert.equal(depositAmount, 2)
+
+    isEnough = await isDepositEnough(client.api, me)
+    assert.isTrue(isEnough)
+  })
+})
