b0580b7e4be432fbb87e36cf29198bbd4e25eda6
Matt Condon <mattgcondon@gmail.com>
2018-02-01 11:38:24 -0800

    fix: deposit tests

--- test/deposit.js
+++ test/deposit.js
@@ -6,15 +6,17 @@ web3.eth.defaultAccount = web3.eth.accounts[0]
 const getContracts = require('../client/util/getContracts')
 
 const makeDeposit = require('../client/claimManager/deposit').makeDeposit
-const isDepositEnough  = require('../client/claimManager/deposit').isDepositEnough
+const isDepositEnough = require('../client/claimManager/deposit').isDepositEnough
 
 describe('Deposit', function () {
+  this.timeout(30000)
+
   let me = web3.eth.accounts[1]
   let client
 
   before(async () => {
-     const contracts = await (await getContracts(web3)).deploy()
-     client = await require('../client')(web3, contracts)
+    const contracts = await (await getContracts(web3)).deploy()
+    client = await require('../client')(web3, contracts)
   })
 
   it('it determines whether the deposit is enough, and makes a deposit', async () => {
