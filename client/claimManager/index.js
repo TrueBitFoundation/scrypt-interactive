f1864039c4ff1ba3862893c61fa6279764f07a1d
Sina Habibian <sinahab@gmail.com>
2018-02-01 18:42:09 -0800

    Implement new challenge primitives

--- client/claimManager/index.js
+++ client/claimManager/index.js
@@ -2,9 +2,11 @@
 const computeStep = require('./computeStep')
 const submit = require('./submit')
 const defend = require('./defend')
+const challenge = require('./challenge')
 
 module.exports = {
   computeStep: computeStep,
   submit: submit,
   defend: defend,
+  challenge: challenge
 }
